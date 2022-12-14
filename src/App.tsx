9b291a156c23d0122ead2f6fdeb76a8345539c93
PannH <pannh2609@gmail.com>
2022-12-14 23:21:55 +0100

    royute

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import {
+   BrowserRouter,
+   Routes,
+   Route
+} from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
 
-   return <h1>Hello from App.tsx</h1>;
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/home' element={ <Home /> } />
+         </Routes>
+      </BrowserRouter>
+   );
 
 }
 
